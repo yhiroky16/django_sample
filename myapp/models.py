20384405ddf89a94f0407048bd23faf4924b022a
yhiroky16 <h.yoshida@oplan.co.jp>
2017-12-10 07:56:50 +0900

    migration変更、表示修正

--- myapp/models.py
+++ myapp/models.py
@@ -24,7 +24,7 @@ class CompanyAssets(models.Model):
     item_name = models.CharField(max_length=100)
     purchas_date = models.CharField(max_length=10)
     buyer = models.CharField(max_length=30)
-    jungle = models.CharField(max_length=1)
+    jungle = models.CharField(max_length=3)
     memo = models.CharField(max_length=500)
     created_date = models.DateTimeField(
             default=timezone.now)
