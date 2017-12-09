f36739459fd13ea849c4ded9f457e358abd4fdfe
yhiroky16 <h.yoshida@oplan.co.jp>
2017-12-10 04:24:02 +0900

    add search action

--- myapp/search_result_views.py
+++ myapp/search_result_views.py
@@ -3,7 +3,15 @@ from django.http import HttpResponse
 from .models import CompanyAssets
 
 def get(request):
+    params = {}
     if request.method == 'GET':
-        item_name = request.POST.get('itemName')
-        jungle = request.POST.get('jungle')
-        
+        item_name = request.GET.get('itemName')
+        if item_name is not None:
+            result = CompanyAssets.objects.filter(item_name__icontains=item_name)
+        else:
+            result = CompanyAssets.objects.all()
+        context = {
+            'result': result,
+            'item_name' : item_name
+        }
+    return render(request, 'search_result.html', context)
--- myapp/urls.py
+++ myapp/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import url
 from . import views
 from . import regist_views
 from . import search_views
+from . import search_result_views
 
 urlpatterns = [
 url(r'^$', views.index, name='index'),
 url(r'^registInput$', regist_views.index, name='index'),
 url(r'^regist$', regist_views.create, name='create'),
 url(r'^searchInput$', search_views.index, name='index'),
+url(r'^search$', search_result_views.get, name='get'),
 ]
